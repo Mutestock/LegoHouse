447639b0076c5cfe4ad589fe56b663709d6d438c
unknown <doublenegative@protonmail.com>
2019-03-23 09:53:06 +0100

    Legohouse can be ordered. Order is assigned to house automatically

--- LegoMansion/src/main/java/PresentationLayer/commands/OrderPageCommand.java
+++ LegoMansion/src/main/java/PresentationLayer/commands/OrderPageCommand.java
@@ -6,9 +6,13 @@
 package PresentationLayer.commands;
 
 import Data.DatamapperImplementation;
+import Logic.FacadeImplementation;
+import Logic.HelperClasses.LegoHelper.Lego;
+import Logic.HelperClasses.OrderHelper.Order;
 import Logic.HelperClasses.UserHelpers.User;
 import PresentationLayer.Command;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,19 +28,49 @@ public class OrderPageCommand extends Command {
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         final DatamapperImplementation dmi = new DatamapperImplementation();
 //
+
         String heightString = request.getParameter("height");
         String widthString = request.getParameter("width");
         String lengthString = request.getParameter("length");
 
-        try {
+        int idLegoCheck = 0;
+        int idOrderCheck = 0;
 
+        try {
             int height = Integer.parseInt(heightString);
             int width = Integer.parseInt(widthString);
             int length = Integer.parseInt(lengthString);
 
-            System.out.println(height);
-            System.out.println(width);
-            System.out.println(length);
+            if (height > 0 && width > 0 && length > 0) {
+               
+                while (dmi.readLego(idLegoCheck).getHeight() != 0) {
+                    idLegoCheck++;
+                }
+                while (!(dmi.readOrder(idOrderCheck).getStatus().isEmpty())) {
+                    idOrderCheck++;
+                }
+                FacadeImplementation fi = new FacadeImplementation();
+                User user = (User) request.getSession().getAttribute("User");
+                Order order = new Order(idOrderCheck, fi.timeString(), "ORDER_RECIEVED", user);
+                dmi.createOrder(order);
+
+                Lego lego = new Lego(idLegoCheck, height, width, length, order);
+                dmi.createLego(lego);
+                if (user == null) {
+                    request.setAttribute("errormessage", "<p style=\"color:red\">User doesn't exist.</p>");
+                    loadJSP(request, response);
+
+                } else {
+                    HttpSession session = request.getSession();
+//                    session.removeAttribute("User");
+////                    session.setAttribute("User", dao.getUser(username));
+//                    session.setAttribute("User", dmi.readUserByEmail(email));
+                    response.sendRedirect("mainpage");
+                }
+            } else {
+                loadJSP(request, response);
+            }
+
             loadJSP(request, response);
         } catch (Exception e) {
             System.out.println("Caught.............!");
